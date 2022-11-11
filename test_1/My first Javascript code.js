6848b2394423f1f677c26e3366f25c47d05cd17a
Faisal-jag <faisal.jag11@gmail.com>
2022-11-11 20:46:13 +0530

    test_2 Declaring Variables.

---
+++ test_1/My first Javascript code.js
@@ -0,0 +1,7 @@
+// Writing My first Javascript code
+// Use the console.log function to output the words "Hello, World".
+console.log("Hello, World!! I Am Faisal");
+
+
+/*Style the output in the console of chrome developer tools. 
+print hello world, font color blue and font size 40px*/
---
+++ test_2/Declaring variables.js
@@ -0,0 +1,25 @@
+// pet name variables
+var petDog = 'Rex';
+var petCat = 'Pepper';
+
+// pet sound variables
+var catSound = "purr";
+var dogSound = "woof";
+
+// output
+console.log(petDog);
+console.log(petCat);
+
+// string + var
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+
+
+// cat sound variable updates
+catSound = "meow";
+
+// output
+console.log(petCat, "now says", catSound);
