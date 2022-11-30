bf61d2e441482ad92198a138b9a34b774b51dd85
Faisal-jag <faisal.jag11@gmail.com>
2022-11-30 19:17:39 +0530

    task2 executed

---
+++ test_4 Practice conditional statements/task2.js
@@ -0,0 +1,41 @@
+/*
+- Code the days of the week program as a switch statement
+1. On the next line, define a new variable, name it day, and set its value to "Sunday".
+
+2. Start coding a switch statement, passing the day variable as the expression to evaluate.
+
+3. Inside the switch, add cases for every day of the week, starting with 'Monday', and ending with 'Sunday'. Make sure to use string values for days. Inside each case, for now, just add a console.log('Do something'), and add a break; on the line below.
+
+4. At the very bottom of the switch statement, add the default case and add a console.log('There is no such day').
+
+5. Finally, update the console.log calls for each case, based on whatever activity you have on each of the days.
+
+*/
+var day = 'Monday';
+switch(day) {
+   case 'Monday':
+       console.log('Read a book');
+       break;
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+   case 'Friday':
+       console.log('Socialize');
+       break;
+   case 'Saturday':
+       console.log('Chill');
+       break;
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+   default:
+       console.log('There is no such day');
+}
+
+//End of task2 code
