f1ebcff839dfcb9fcf7d95c6da6c1cbc32bda091
Faisal-jag <faisal.jag11@gmail.com>
2022-11-30 19:52:50 +0530

    task5 executed

---
+++ test_5 Repetitive tasks with loops/task5.js
@@ -0,0 +1,22 @@
+
+
+/*
+Task 5
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+*/
+
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+};
+
+
+//End of task5 code
