6eb5489fb354759dbb181944f68f55c37ede36b1
Faisal-jag <faisal.jag11@gmail.com>
2022-11-30 19:51:59 +0530

    task4 executed

---
+++ test_5 Repetitive tasks with loops/task4.js
@@ -0,0 +1,24 @@
+
+
+/*
+Task 4
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+*/
+
+
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i = i - 1;
+};
+console.log('Counting completed!');
+
+//End of task4 code
