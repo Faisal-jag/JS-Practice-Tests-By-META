ddb946cf99fb92658b48a3ab2ac28bcfcd544c25
Faisal-jag <faisal.jag11@gmail.com>
2022-12-11 07:05:45 +0530

    tested with diff ip text

--- test_1/js_test_1.js
+++ test_1/js_test_1.js
@@ -1,6 +1,6 @@
 // Writing My first Javascript code
 // Use the console.log function to output the words "Hello, World".
-console.log("Hello, World!! I Am Faisal");
+console.log("Hello, World!! I Am Faisal jag");
 
 
 /*Style the output in the console of chrome developer tools. 
